1b1647ea5d2d77cf4aff455195fe188840867f48
K. Bala Murali <k.balamurali0701@gmail.com>
2025-03-23 19:40:57 +0530

    Used mapping is with some bugs

---
+++ src/pages/javascript/Javascript.App.tsx
@@ -0,0 +1,9 @@
+import CoreJavascriptCheat from './../../components/javascript/cheats/core.javascript.cheat.component.tsx'
+
+function JavascriptApp () {
+    return (
+        <CoreJavascriptCheat />
+    )
+}
+
+export default JavascriptApp
